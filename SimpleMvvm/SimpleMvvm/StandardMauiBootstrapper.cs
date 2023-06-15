b401852f978bf094ebe42f19e077571b70920bed
Pawel Gajzler <pawel.gajzler.pg@gmail.com>
2023-06-15 18:10:16 +0200

    added bootstrapper for default maui ioc container

---
+++ SimpleMvvm/SimpleMvvm/StandardMauiBootstrapper.cs
@@ -0,0 +1,11 @@
+﻿using SimpleMvvm.Messaging;
+namespace SimpleMvvm
+{
+    public static class StandardMauiBootstrapper
+    {
+        public static void AddSimpleMvvm(this MauiAppBuilder builder)
+        {
+            builder.Services.AddSingleton<IMessenger, Messenger>();
+        }
+    }
+}
